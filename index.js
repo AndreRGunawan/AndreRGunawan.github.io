b73896fa0ddacdf263897ab5569e20f6e8a79b50
AndreRGunawan <andre.graphica@gmail.com>
2020-01-31 15:49:31 +0700

    Initial upload for final project

---
+++ index.js
@@ -0,0 +1,115 @@
+// console.log("hi")
+
+function buttonScene1() {
+    document.getElementById("scene-1").style.display = "flex";
+    document.getElementById("scene-2").style.display = "none" // lawan dari none adalah block
+    document.getElementById("scene-3").style.display = "none";
+    document.getElementById("scene-4").style.display = "none";
+    document.getElementById("scene-5").style.display = "none";
+    document.getElementById("scene-6").style.display = "none";
+    document.getElementById("scene-7").style.display = "none";
+    document.getElementById("scene-8").style.display = "none";
+
+    
+    // elem.dispatchEvent(event)
+}
+
+function buttonScene2() {
+    document.getElementById("scene-2").style.display = "flex" // lawan dari none adalah block
+    document.getElementById("scene-1").style.display = "none" // lawan dari none adalah block
+    document.getElementById("scene-3").style.display = "none";
+    document.getElementById("scene-4").style.display = "none";
+    document.getElementById("scene-5").style.display = "none";
+    document.getElementById("scene-6").style.display = "none";
+    document.getElementById("scene-7").style.display = "none";
+    document.getElementById("scene-8").style.display = "none";
+    
+    // elem.dispatchEvent(event)
+}
+
+function buttonScene3() {
+    document.getElementById("scene-3").style.display = "flex" // lawan dari none adalah block
+    document.getElementById("scene-1").style.display = "none" // lawan dari none adalah block
+    document.getElementById("scene-2").style.display = "none";
+    document.getElementById("scene-4").style.display = "none";
+    document.getElementById("scene-5").style.display = "none";
+    document.getElementById("scene-6").style.display = "none";
+    document.getElementById("scene-7").style.display = "none";
+    document.getElementById("scene-8").style.display = "none";
+    
+}
+
+function buttonScene4() {
+    document.getElementById("scene-4").style.display = "flex" // lawan dari none adalah block
+    document.getElementById("scene-1").style.display = "none" // lawan dari none adalah block
+    document.getElementById("scene-2").style.display = "none";
+    document.getElementById("scene-3").style.display = "none";
+    document.getElementById("scene-5").style.display = "none";
+    document.getElementById("scene-6").style.display = "none";
+    document.getElementById("scene-7").style.display = "none";
+    document.getElementById("scene-8").style.display = "none";
+    
+}
+
+function buttonScene5() {
+    document.getElementById("scene-5").style.display = "flex" // lawan dari none adalah block
+    document.getElementById("scene-1").style.display = "none" // lawan dari none adalah block
+    document.getElementById("scene-2").style.display = "none";
+    document.getElementById("scene-3").style.display = "none";
+    document.getElementById("scene-4").style.display = "none";
+    document.getElementById("scene-6").style.display = "none";
+    document.getElementById("scene-7").style.display = "none";
+    document.getElementById("scene-8").style.display = "none";
+    
+}
+
+function buttonScene6() {
+    document.getElementById("scene-6").style.display = "flex" // lawan dari none adalah block
+    document.getElementById("scene-1").style.display = "none" // lawan dari none adalah block
+    document.getElementById("scene-2").style.display = "none";
+    document.getElementById("scene-3").style.display = "none";
+    document.getElementById("scene-4").style.display = "none";
+    document.getElementById("scene-5").style.display = "none";
+    document.getElementById("scene-7").style.display = "none";
+    document.getElementById("scene-8").style.display = "none";
+    
+}
+
+function buttonScene7() {
+    document.getElementById("scene-7").style.display = "flex" // lawan dari none adalah block
+    document.getElementById("scene-1").style.display = "none" // lawan dari none adalah block
+    document.getElementById("scene-2").style.display = "none";
+    document.getElementById("scene-3").style.display = "none";
+    document.getElementById("scene-4").style.display = "none";
+    document.getElementById("scene-5").style.display = "none";
+    document.getElementById("scene-6").style.display = "none";
+    document.getElementById("scene-8").style.display = "none";
+    
+}
+
+function buttonScene8() {
+    document.getElementById("scene-8").style.display = "flex" // lawan dari none adalah block
+    document.getElementById("scene-1").style.display = "none" // lawan dari none adalah block
+    document.getElementById("scene-2").style.display = "none";
+    document.getElementById("scene-3").style.display = "none";
+    document.getElementById("scene-4").style.display = "none";
+    document.getElementById("scene-5").style.display = "none";
+    document.getElementById("scene-6").style.display = "none";
+    document.getElementById("scene-7").style.display = "none";
+    
+}
+
+// function buttonScene3() {
+//     document.getElementById("scene-1").style.display = "none";
+//     document.getElementById("scene-2").style.display = "block" // lawan dari none adalah block
+
+    
+//     // elem.dispatchEvent(event)
+// }
+
+// document.getElementById("buttonScene-2").addEventListener("click", buttonScene2()) 
+//dua parameter, ketrigger kapan dan apa efeknya
+//akses selalu di luar function
+
+// elem.displayElementbyId
+
